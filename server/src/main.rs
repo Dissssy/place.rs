6361e3600e1232fce74823dff1d2fbc3691cfe5e
Dissy <you@willsh.art>
2022-12-03 18:31:06 -0500

    Initial

---
+++ server/src/main.rs
@@ -0,0 +1,242 @@
+use actix::fut::wrap_future;
+use actix::prelude::*;
+use actix_web_actors::ws;
+use serde::{Deserialize, Serialize};
+use sha2::Digest;
+use sha2::Sha256;
+// use sqlx::postgres::PgPoolOptions;
+use std::sync::Arc;
+use tokio::sync::mpsc::UnboundedReceiver;
+use tokio::sync::oneshot;
+use tokio::sync::Mutex;
+
+use actix_web::{get, web, App, HttpServer, Responder};
+use place_rs_shared::{Color, Pixel, Place, ServerConfig, User, XY};
+
+#[get("/api/canvas/{x}/{y}")]
+async fn api_canvas(data: web::Data<Arc<Mutex<Place>>>, coord: web::Path<(usize, usize)>) -> impl Responder {
+    // serve the current state of the canvas as json
+    // we will take advantage of chunking to make this a faster process, x and y are the 128x128 chunk coordinates
+    let data = data.lock().await;
+    let chunk = data.get_chunk(coord.0, coord.1);
+    web::Json(chunk)
+}
+
+#[get("/api/info")]
+async fn api_info() -> impl Responder {
+    let config = ServerConfig::load().unwrap();
+    let info = SafeInfo {
+        timeout: config.timeout,
+        size: config.size,
+    };
+    web::Json(info)
+}
+
+#[derive(Serialize, Deserialize)]
+struct SafeInfo {
+    timeout: i64,
+    size: XY,
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let config = ServerConfig::load().unwrap();
+    // we will run a websocket server on the same port as the http server under the /ws path, this will serve live events and updates to the client, specifically when a pixel changes, and the current timeout for the user. the user will push messages to this websocket to update a pixel
+    // we will run a http server on the same port as the websocket server under the /api path, this will serve the basic api endpoints to get the current state of the canvas, to get the user up to date
+    let place = Arc::new(Mutex::new(Place::load().await.unwrap()));
+    let place_clone = place.clone();
+    println!("Starting server on port {}", config.port);
+    let x = HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(place_clone.clone()))
+            .service(api_canvas)
+            .service(api_info)
+            .route("/ws/", web::get().to(ws_index))
+    })
+    .bind((config.host, config.port))?
+    .run()
+    .await;
+    println!("{:?}", x);
+    println!("Shutting down server");
+    println!("Saving canvas");
+    let place = place.lock().await;
+    println!("{:?}", place.handler.save(place.get_save_data()).await);
+
+    x
+}
+
+use actix::{Actor, StreamHandler};
+use actix_web::{HttpRequest, HttpResponse};
+
+/// Define HTTP actor
+struct MyWs {
+    place: Arc<Mutex<Place>>,
+    rx: Option<UnboundedReceiver<WebsocketMessage>>,
+    id: String,
+}
+
+impl Actor for MyWs {
+    // we will need two way async communication with the websocket, so we will use an unbounded channel
+    type Context = ws::WebsocketContext<Self>;
+}
+
+// we need to be able to handle messages FROM the websocket, and push messages TO the websocket
+// we will use the WebSocketMessage enum from the shared crate to handle this
+use place_rs_shared::WebsocketMessage;
+
+async fn ws_index(req: HttpRequest, stream: web::Payload, data: web::Data<Arc<Mutex<Place>>>) -> Result<HttpResponse, actix_web::Error> {
+    let place = data.get_ref().clone();
+    let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
+    place.lock().await.add_websocket(hash(req.peer_addr().unwrap().ip().to_string()), tx);
+    let myws = MyWs {
+        place,
+        rx: Some(rx),
+        id: hash(req.peer_addr().unwrap().ip().to_string()),
+    };
+    ws::start(myws, &req, stream)
+}
+
+// listen for messages from the websocket and handle them, while using heartbeat to keep the connection alive
+// #[async_trait::async_trait]
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) => {
+                // let th = RawWebsocketMessage::PixelUpdate {
+                //     x: 0,
+                //     y: 0,
+                //     color: Color { r: 0, g: 0, b: 0 },
+                // };
+                // println!("json: {}", serde_json::to_string(&th).unwrap());
+                // we will use the WebsocketMessage enum from the shared crate to handle this
+                let msg = serde_json::from_str::<RawWebsocketMessage>(&text);
+                if let Ok(msg) = msg {
+                    match msg {
+                        RawWebsocketMessage::PixelUpdate { x, y, color } => {
+                            let time = chrono::Utc::now().timestamp();
+                            let pixel = Pixel {
+                                location: XY { x, y },
+                                color,
+                                timestamp: Some(time),
+                                user: Some(User {
+                                    id: hash(self.id.clone()),
+                                    name: "test".to_string(),
+                                    timeout: time,
+                                }),
+                            };
+                            let (tx, mut rx) = oneshot::channel::<String>();
+                            let f = wrap_future(update_pixel(pixel, self.place.clone(), tx));
+                            ctx.spawn(f);
+                            ctx.run_interval(std::time::Duration::from_millis(100), move |_act, ctx| {
+                                if let Ok(msg) = rx.try_recv() {
+                                    ctx.text(msg);
+                                }
+                            });
+                        }
+                        RawWebsocketMessage::Heartbeat => {
+                            // send the heartbeat to the websocket
+                            ctx.text("BeatHeart");
+                        }
+                        RawWebsocketMessage::Listen => {
+                            // check if the handle is already set, if it is, return an error message
+
+                            let rx = self.rx.take();
+                            if let Some(mut rx) = rx {
+                                ctx.run_interval(std::time::Duration::from_millis(100), move |_act, ctx| {
+                                    // check if the websocket is still connected
+                                    if !ctx.state().alive() {
+                                        // if it is not, remove the websocket from the place
+                                        // act.place.lock().await.remove_websocket(hash(act.id.clone()));
+                                        ctx.stop();
+                                        return;
+                                    }
+                                    // check if there is a message in the channel
+                                    if let Ok(msg) = rx.try_recv() {
+                                        // if there is, send it to the websocket
+                                        ctx.text(serde_json::to_string(&msg).unwrap());
+                                    }
+                                });
+                                ctx.text("Listening");
+                                // let (mtx, mut mrx) = tokio::sync::mpsc::unbounded_channel::<String>();
+                                // let (ktx, mut krx) = tokio::sync::oneshot::channel::<()>();
+                                // let _: Promise<()> = Promise::spawn_thread(format!("listener:{}", self.ip), move || {
+                                //     loop {
+                                //         if let Ok(()) = krx.try_recv() {
+                                //             break;
+                                //         }
+                                //         // check if there is a message in the channel
+                                //         if let Ok(msg) = rx.try_recv() {
+                                //             // send the message to the websocket
+                                //             mtx.send(serde_json::to_string(&msg).unwrap()).unwrap();
+                                //         }
+                                //         // sleep for 100ms
+                                //         std::thread::sleep();
+                                //     }
+                                // });
+                            } else {
+                                ctx.text(
+                                    serde_json::to_string(&RawWebsocketMessage::Error {
+                                        message: "Already listening".to_string(),
+                                    })
+                                    .unwrap(),
+                                );
+                            }
+                        }
+                        _ => {
+                            ctx.text(
+                                serde_json::to_string(&RawWebsocketMessage::Error {
+                                    message: "Unknown message".to_string(),
+                                })
+                                .unwrap(),
+                            );
+                        }
+                    }
+                } else {
+                    ctx.text(
+                        serde_json::to_string(&RawWebsocketMessage::Error {
+                            message: "Invalid message".to_string(),
+                        })
+                        .unwrap(),
+                    );
+                }
+            }
+            Ok(ws::Message::Close(reason)) => {
+                // remove the websocket from the place
+                // let mut place = self.place.lock().await;
+                // place.remove_websocket(ctx.address());
+                ctx.close(reason);
+                ctx.stop();
+            }
+            _ => ctx.stop(),
+        }
+    }
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+enum RawWebsocketMessage {
+    PixelUpdate { x: usize, y: usize, color: Color },
+    Heartbeat,
+    Error { message: String },
+    Listen,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+struct Msg {
+    raw: RawWebsocketMessage,
+}
+
+fn hash(s: String) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(s);
+    let result = hasher.finalize();
+    format!("{:x}", result)
+}
+
+async fn update_pixel(pixel: Pixel, place: Arc<Mutex<Place>>, tx: oneshot::Sender<String>) {
+    let mut place = place.lock().await;
+    let r = place.set_pixel(pixel).await;
+    if let Err(r) = r {
+        tx.send(r.to_string()).unwrap();
+    }
+}
---
+++ shared/src/lib.rs
@@ -0,0 +1,493 @@
+use anyhow::{anyhow, Error};
+use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres, Row};
+use std::{
+    fmt::{Display, Formatter},
+    io::Write,
+    path::PathBuf,
+    str::FromStr,
+};
+use tokio::sync::mpsc::UnboundedSender;
+
+pub struct Place {
+    pub data: Vec<Vec<Pixel>>,
+
+    pub users: Vec<User>,
+    pub handler: Box<dyn Handler>,
+    pub websockets: Vec<WebsocketHandler>,
+    pub config: ServerConfig,
+}
+
+impl Clone for Place {
+    fn clone(&self) -> Self {
+        Self {
+            data: self.data.clone(),
+            users: self.users.clone(),
+            handler: self.handler.clone(),
+            websockets: vec![],
+            config: self.config.clone(),
+        }
+    }
+}
+
+pub struct WebsocketHandler {
+    pub id: String,
+    pub handle: UnboundedSender<WebsocketMessage>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum WebsocketMessage {
+    Update(Pixel),
+}
+
+impl Place {
+    pub async fn load() -> Result<Self, Error> {
+        let config = ServerConfig::load()?;
+        let p: Box<dyn Handler> = match config.handler {
+            HandlerType::Json => Box::new(JsonHandler::new()),
+            HandlerType::Postgres(data) => Box::new(PostgresHandler {
+                pool: PgPoolOptions::new().max_connections(5).connect(&get_pg_uri(data.clone())).await?,
+                data,
+            }),
+            // _ => unreachable!("Handler not implemented"),
+        };
+        let p = p.load().await;
+        if let Ok(p) = p {
+            Ok(p)
+        } else {
+            Err(p.err().unwrap())
+        }
+    }
+    pub fn get_save_data(&self) -> SaveData {
+        SaveData {
+            data: self.data.clone(),
+            users: self.users.clone(),
+        }
+    }
+    pub fn get_chunk(&self, x: usize, y: usize) -> Vec<Pixel> {
+        let mut chunk: Vec<Pixel> = Vec::new();
+        for i in 0..self.config.chunk_size {
+            for j in 0..self.config.chunk_size {
+                let pixel = self.data.get(x * self.config.chunk_size + i).and_then(|row| row.get(y * self.config.chunk_size + j));
+                if let Some(pixel) = pixel {
+                    chunk.push(pixel.clone());
+                }
+            }
+        }
+        chunk
+    }
+    pub async fn set_pixel(&mut self, newpixel: Pixel) -> Result<(), Error> {
+        let now = chrono::Utc::now();
+        let thisnow = now.timestamp();
+        let user = self.users.iter_mut().find(|user| if let Some(theuser) = newpixel.user.clone() { theuser.id == user.id } else { false });
+        if let Some(user) = user.as_ref() {
+            if user.timeout > thisnow {
+                return Err(anyhow::anyhow!("User is within timeout. Please wait {} seconds", user.timeout - thisnow));
+            }
+        }
+
+        let row = self.data.get_mut(newpixel.location.x).ok_or_else(|| anyhow::anyhow!("Pixel x out of bounds"))?;
+        let pixel = row.get_mut(newpixel.location.y).ok_or_else(|| anyhow::anyhow!("Pixel y out of bounds"))?;
+
+        if pixel.color == newpixel.color {
+            return Err(anyhow::anyhow!("Pixel color is the same, paint not wasted"));
+        }
+
+        *pixel = newpixel.clone();
+
+        for websocket in self.websockets.iter_mut() {
+            let r = websocket.handle.send(WebsocketMessage::Update(newpixel.clone()));
+            if let Err(e) = r {
+                println!("Error sending websocket message: {}", e);
+            }
+        }
+        self.websockets.retain(|websocket| !websocket.handle.is_closed());
+
+        if let Some(user) = user {
+            user.timeout = thisnow + self.config.timeout;
+        } else {
+            let mut thisuser = newpixel.user.as_ref().unwrap().clone();
+            thisuser.timeout = thisnow + self.config.timeout;
+            self.users.push(thisuser);
+        }
+        self.save_pixel(newpixel).await?;
+        Ok(())
+    }
+    pub fn add_websocket(&mut self, id: String, handle: UnboundedSender<WebsocketMessage>) {
+        self.websockets.push(WebsocketHandler { id, handle });
+    }
+    pub fn remove_websocket(&mut self, id: String) {
+        self.websockets.retain(|websocket| websocket.id != id);
+    }
+    pub async fn save_pixel(&self, pixel: Pixel) -> Result<(), Error> {
+        self.handler.save_pixel(pixel).await
+    }
+}
+
+fn get_blank_data(size: XY) -> Vec<Vec<Pixel>> {
+    let mut data = Vec::new();
+    for y in 0..size.y {
+        let mut row = Vec::new();
+        for x in 0..size.x {
+            row.push(Pixel::new(x, y));
+        }
+        data.push(row);
+    }
+    data
+}
+
+pub struct JsonHandler;
+
+#[async_trait]
+impl Handler for JsonHandler {
+    async fn load(&self) -> Result<Place, Error> {
+        let mut path = get_data_path();
+        path.push("data.json");
+        let config = ServerConfig::load()?;
+
+        if !path.exists() {
+            let data = get_blank_data(get_size());
+            let place = Place {
+                data,
+                users: vec![],
+                handler: Box::new(Self::new()),
+                websockets: Vec::new(),
+                config,
+            };
+
+            let p = place.handler.save(place.get_save_data());
+            if p.await.is_ok() {
+                Ok(place)
+            } else {
+                Err(anyhow!("Error saving"))
+            }
+        } else {
+            let file = std::fs::File::open(path)?;
+            let data: SaveData = serde_json::from_reader(file)?;
+            Ok(Place {
+                data: data.data,
+                users: data.users,
+                handler: Box::new(Self::new()),
+                websockets: Vec::new(),
+                config,
+            })
+        }
+    }
+    async fn save(&self, data: SaveData) -> Result<(), Error> {
+        let mut path = get_data_path();
+        path.push("data.json");
+        let file = std::fs::File::create(path)?;
+        serde_json::to_writer_pretty(file, &data)?;
+        Ok(())
+    }
+    async fn save_pixel(&self, _pixel: Pixel) -> Result<(), Error> {
+        Ok(())
+    }
+    fn clone(&self) -> Box<dyn Handler> {
+        Box::new(Self::new())
+    }
+}
+
+pub struct PostgresHandler {
+    data: PostgresConfig,
+    pool: Pool<Postgres>,
+}
+
+fn get_pg_uri(data: PostgresConfig) -> String {
+    format!("postgres://{}:{}@{}:{}/{}", data.username, data.password, data.host, data.port, data.database)
+}
+
+#[async_trait]
+impl Handler for PostgresHandler {
+    async fn save(&self, _data: SaveData) -> Result<(), Error> {
+        Ok(())
+    }
+
+    async fn load(&self) -> Result<Place, Error> {
+        println!("Loading from postgres");
+        let config = ServerConfig::load()?;
+
+        let mut data = get_blank_data(config.size);
+
+        let mut users = Vec::new();
+        println!("Loading data");
+        let mut count = 0;
+        let rows = sqlx::query("SELECT * FROM data").fetch_all(&self.pool).await?;
+        for row in rows {
+            let x: i32 = row.get("x");
+            let x = x as usize;
+            let y: i32 = row.get("y");
+            let y = y as usize;
+            let timeout: i32 = row.get("user_timeout");
+            let timeout = timeout as i64;
+            let timestamp: i32 = row.get("timestamp");
+            let timestamp = Some(timestamp as i64);
+            let pixel = Pixel {
+                location: XY { x, y },
+                color: Color::from_str(row.get("color"))?,
+                user: Some(User {
+                    id: row.get("user_id"),
+                    timeout,
+                    name: row.get("user_name"),
+                }),
+                timestamp,
+            };
+
+            let row = data.get_mut(x);
+            if let Some(row) = row {
+                let tpixel = row.get_mut(y);
+                if let Some(tpixel) = tpixel {
+                    users.push(pixel.user.as_ref().unwrap().clone());
+                    *tpixel = pixel.clone();
+                    count += 1;
+                }
+            }
+        }
+        println!("Loaded {} pixels", count);
+
+        users.sort_by(|a, b| b.timeout.cmp(&a.timeout));
+        users.dedup_by(|a, b| a.id == b.id);
+        println!("Loaded {} users", users.len());
+
+        Ok(Place {
+            data,
+            users,
+            handler: self.clone(),
+            websockets: Vec::new(),
+            config,
+        })
+    }
+
+    async fn save_pixel(&self, pixel: Pixel) -> Result<(), Error> {
+        sqlx::query("INSERT INTO data (x, y, color, user_id, user_name, user_timeout, timestamp, uuid) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) ON CONFLICT (uuid) DO UPDATE SET color = $3, user_id = $4, user_name = $5, user_timeout = $6, timestamp = $7").bind(pixel.location.x as i64).bind(pixel.location.y as i64).bind(&(pixel.color.to_string())).bind(&pixel.user.as_ref().unwrap().id).bind(&pixel.user.as_ref().unwrap().name).bind(pixel.user.as_ref().unwrap().timeout).bind(pixel.timestamp).bind(format!("{}:{}", pixel.location.x, pixel.location.y)).execute(&self.pool).await?;
+        Ok(())
+    }
+
+    fn clone(&self) -> Box<dyn Handler> {
+        Box::new(Self {
+            data: self.data.clone(),
+            pool: self.pool.clone(),
+        })
+    }
+}
+
+pub fn get_size() -> XY {
+    let config = ServerConfig::load().unwrap();
+    config.size
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SaveData {
+    pub data: Vec<Vec<Pixel>>,
+    pub users: Vec<User>,
+}
+
+impl JsonHandler {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl Default for JsonHandler {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Pixel {
+    pub color: Color,
+
+    pub user: Option<User>,
+    pub location: XY,
+    pub timestamp: Option<i64>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Copy)]
+pub struct XY {
+    pub x: usize,
+    pub y: usize,
+}
+
+impl Pixel {
+    fn new(x: usize, y: usize) -> Self {
+        Self {
+            color: Color::default(),
+            user: None,
+            location: XY { x, y },
+            timestamp: None,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct User {
+    pub name: String,
+    pub id: String,
+    pub timeout: i64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+impl FromStr for Color {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut chars = s.chars();
+        let r = u8::from_str_radix(&chars.by_ref().take(2).collect::<String>(), 16)?;
+        let g = u8::from_str_radix(&chars.by_ref().take(2).collect::<String>(), 16)?;
+        let b = u8::from_str_radix(&chars.by_ref().take(2).collect::<String>(), 16)?;
+        Ok(Self { r, g, b })
+    }
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:02x}{:02x}{:02x}", self.r, self.g, self.b)
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ServerConfig {
+    pub host: String,
+    pub port: u16,
+    pub size: XY,
+    pub handler: HandlerType,
+    pub timeout: i64,
+    pub chunk_size: usize,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct ServerConfigGetting {
+    host: Option<String>,
+    port: Option<u16>,
+    size: Option<XY>,
+    handler: Option<HandlerType>,
+    timeout: Option<i64>,
+    chunk_size: Option<usize>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+
+pub enum HandlerType {
+    Json,
+    Postgres(PostgresConfig),
+}
+
+#[async_trait]
+pub trait Handler: Send {
+    async fn save(&self, data: SaveData) -> Result<(), Error>;
+    async fn load(&self) -> Result<Place, Error>;
+    async fn save_pixel(&self, pixel: Pixel) -> Result<(), Error>;
+    fn clone(&self) -> Box<dyn Handler>;
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PostgresConfig {
+    pub host: String,
+    pub port: u16,
+    pub username: String,
+    pub password: String,
+    pub database: String,
+}
+
+impl FromStr for HandlerType {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "json" => Ok(HandlerType::Json),
+            "postgres" => Ok(HandlerType::Postgres(PostgresConfig {
+                host: safe_getter(None, "Postgres host: "),
+                port: safe_getter(None, "Postgres port: "),
+                username: safe_getter(None, "Postgres username: "),
+                password: safe_getter(None, "Postgres password: "),
+                database: safe_getter(None, "Postgres database: "),
+            })),
+            _ => Err(anyhow::anyhow!("Invalid handler")),
+        }
+    }
+}
+
+impl ServerConfig {
+    pub fn load() -> Result<Self, Error> {
+        let mut config_path = get_data_path();
+
+        std::fs::create_dir_all(&config_path)?;
+        config_path.push("server_config.json");
+
+        let config_getting: ServerConfigGetting = match std::fs::read_to_string(&config_path) {
+            Ok(config) => match serde_json::from_str::<ServerConfigGetting>(&config) {
+                Ok(config) => config,
+                Err(e) => {
+                    eprintln!("Error deserializing server config: {}", e);
+                    ServerConfigGetting {
+                        host: None,
+                        port: None,
+                        handler: None,
+                        timeout: None,
+                        chunk_size: None,
+                        size: None,
+                    }
+                }
+            },
+            Err(e) => {
+                eprintln!("Error reading server config: {}", e);
+                ServerConfigGetting {
+                    host: None,
+                    port: None,
+                    handler: None,
+                    timeout: None,
+                    chunk_size: None,
+                    size: None,
+                }
+            }
+        };
+        let item = Self {
+            host: safe_getter(config_getting.host, "Input host: "),
+            port: safe_getter(config_getting.port, "Input port: "),
+            handler: safe_getter(config_getting.handler, "Input handler: "),
+            timeout: safe_getter(config_getting.timeout, "Input timeout: "),
+            chunk_size: safe_getter(config_getting.chunk_size, "Input chunk size: "),
+            size: XY {
+                x: safe_getter(config_getting.size.map(|x| x.x), "Input width: "),
+                y: safe_getter(config_getting.size.map(|x| x.y), "Input height: "),
+            },
+        };
+
+        let mut file = std::fs::File::create(&config_path)?;
+        file.write_all(serde_json::to_string_pretty(&item)?.as_bytes())?;
+        Ok(item)
+    }
+}
+
+fn safe_getter<T: std::str::FromStr>(value: Option<T>, prompt: &str) -> T {
+    match value {
+        Some(value) => value,
+        None => {
+            let mut input = String::new();
+            loop {
+                print!("{}", prompt);
+                std::io::stdout().flush().unwrap();
+                std::io::stdin().read_line(&mut input).unwrap();
+                match input.trim().parse::<T>() {
+                    Ok(value) => break value,
+                    Err(_) => {
+                        println!("Invalid input");
+                        input.clear();
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn get_data_path() -> PathBuf {
+    dirs::config_dir().unwrap_or_else(|| PathBuf::from_str("./").unwrap()).join("place_rs")
+}
